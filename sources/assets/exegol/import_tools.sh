5b40eb2fbbf8ffbbe3817915269567291110c796
QU35T-code <github@qu35t-mail.simplelogin.com>
2023-08-19 21:36:14 +0200

    Fix import tools script

--- sources/assets/exegol/import_tools.sh
+++ sources/assets/exegol/import_tools.sh
@@ -2,10 +2,10 @@
 
 echo "Import tools"
 
-cp -r /tmp/resources/folder-opt/ /opt/tools/
-cp -r /tmp/resources/folder-ruby/ /usr/local/rvm/gems/
-cp -r /tmp/resources/folder-go/ /root/go/bin/
-cp -r /tmp/resources/folder-pipx/ /root/.local/pipx/venvs/
+cp -r /tmp/resources/folder-opt/* /opt/tools/
+cp -r /tmp/resources/folder-ruby/* /usr/local/rvm/gems/
+cp -r /tmp/resources/folder-go/* /root/go/bin/
+cp -r /tmp/resources/folder-pipx/* /root/.local/pipx/venvs/
 
 cat /tmp/resources/aliases-file >> /opt/.exegol_aliases
 cat /tmp/resources/history-file >> /root/.zsh_history
